4cd6e47bb9bc795efea3b2d1a08dd77901f5a253
rlhw22 <rlhw11@hotmail.com>
2025-03-21 16:10:53 +0800

    adding folder vpc resources

---
+++ terraform/Exercise2/Instance.tf
@@ -0,0 +1,13 @@
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.amiID.id
+  instance_type          = "t3.micro"
+  key_name               = "dove-key"
+  vpc_security_group_ids = [aws_security_group.dove-sg.id]
+  availability_zone      = "ap-southeast-1a"
+
+  tags = {
+    Name    = "Dove-web"
+    Project = "Dove"
+  }
+}
---
+++ terraform/Exercise2/Keypair.tf
@@ -0,0 +1,4 @@
+resource "aws_key_pair" "dove-key" {
+  key_name   = "dove-key"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMjqcFPe0G4oeiWY87DwLSvCV+HZsrCVGsRMDD1jHrCx rlhw1@LAPTOP-SM2PKU8C"
+}
